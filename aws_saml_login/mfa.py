a299fcf6b7d9bec60bff05f6415b7787e103d0cf
Walker Wheeler <wswheeler@alaska.edu>
2017-10-20 10:10:54 -0800

    feat: modifies duo object creation
    This modifications allows for the the duo object to be
    created with the attributes, so that the object is ready
    to be processed as soon as it is created, without
    requiring reparsing of the data.

--- aws_saml_login/mfa.py
+++ aws_saml_login/mfa.py
@@ -1,4 +1,5 @@
 import re
+from bs4 import BeautifulSoup
 
 class MfaNone(object):
 
@@ -16,32 +17,57 @@ class Duo(object):
 
     @staticmethod
     def detect(response,session):
-        if DuoScript.isFound(response):
-            return DuoScript(response,session)
-        elif DuoIframe.isFound(response):
-            return DuoIframe(response,session)
-        return MfaNone(response,session)
+        attributes = DuoScript.findAttributes(response)
+        if ( len(attributes) > 0 ):
+            return DuoScript(response, session, attributes)
+
+        attributes = DuoIframe.findAttributes(response)
+        if (len(attributes) > 0):
+            return DuoIframe(response, session, attributes)
+
+        return MfaNone(response, session)
 
 class DuoScript(Duo):
     @staticmethod
-    def isFound(response):
-        duoMatch = re.search('Duo\.init\(({.*})\);', response.text, re.DOTALL)
-        return duoMatch is not None
+    def findAttributes(response):
+        attributes = {}
+        print( len(attributes) > 0 )
+        duoMatch = re.search('Duo\.init\(.*({.*}).*\);', response.text, re.DOTALL)
+        if duoMatch:
+            attributes = duoMatch.group(1)
+        return attributes
 
-    def __init__(self, response,session):
+    def __init__(self, response,session, attributes):
         self.response = response
         self.session = session
+        self.attributes = attributes
 
     def process(self):
         return self.response
 
 class DuoIframe(Duo):
     @staticmethod
-    def isFound(response):
-        duoMatch = re.search('(<iframe id="duo_iframe".*>.*</iframe>)', response.text, re.DOTALL)
-        return duoMatch is not None
+    def findAttributes(response):
+        attributes = {}
+        duoMatch = re.search('(<iframe .*"duo_iframe".*>.*</iframe>)',
+                             response.text, re.DOTALL)
+        if duoMatch:
+            attributes = DuoIframe.getDuoAttributesFromFrameElement(duoMatch.group(1))
+        return attributes
 
-    def __init__(self,response, session):
+    @staticmethod
+    def getDuoAttributesFromFrameElement(match):
+        attributes = {}
+        for iframe in BeautifulSoup(match, 'html.parser'):
+            for key in iframe.attrs.keys():
+                newkey = key.replace('data-','')
+                newkey = newkey.replace('-','_')
+                newkey = newkey.replace('id','iframe')
+                attributes[newkey] = iframe.attrs[key]
+        return attributes
+
+
+    def __init__(self,response, session, attributes):
         self.response = response
         self.session = session
 
